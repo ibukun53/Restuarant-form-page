d1a28c75d0855f2326d3295fd7ea2dbabe5427c5
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-04-27 18:11:06 +0100

    Adding comment message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// validate text
 const validateText = () =>{
     let x= document.getElementById('fname');
     let a= document.getElementById('city');
@@ -8,17 +9,17 @@ const validateText = () =>{
     b.value= b.value.toUpperCase();
     y.value= y.value.toUpperCase();
 }
-
+// validate email
 const validateEmail = (email) =>{
     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         return mailFormat.test(email);
     }
-
+// validate phone number
     const validatePhone = (phone) =>{
         const phoneFormat = /^(\+91-|\+91|0)?\d{10}$/;
             return phoneFormat.test(phone);
         }
-
+// clicking the submit will signify the detail that is not validated
  const doValidate =()=>{
     if(!validateEmail(document.getElementById('email').value)||!validatePhone(document.getElementById('password').value) ){
         alert('Invalid Email');
